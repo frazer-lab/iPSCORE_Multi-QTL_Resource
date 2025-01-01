a4d9d795a82a857937ad7a2ad5f4a383309cad72
tdarthur40 <tdarthur40@github.com>
2025-01-01 07:25:13 -0800

    deleted notebook checkpoints

--- Analyses/.ipynb_checkpoints/03.3b.run.get_qtl_stats_for_randvars-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-# Author: Jennifer Nguyen
-
-tissue=$1
-qtl_list=$2
-date=$3
-Rscript /projects/CARDIPS/analysis/epigenome_resource/analyses/jennifer/ipscore_unique_qtls/mashr/scripts/get_qtl_stats_for_randvars.R --tissue $tissue --qtl_list $qtl_list --date $date
